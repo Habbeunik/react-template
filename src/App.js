82b9c63d13d4695ca4d98685cd0497657020bce4
habbey sesan <habbeyunik@gmail.com>
2019-10-25 13:50:13 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
+const HomePage = React.lazy(() => import('./pages/home'));
+const AboutPage = React.lazy(() => import('./pages/about'));
 
 /**
  * @params { Object } props
@@ -8,22 +12,14 @@ import './App.css';
  */
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Router>
+			<React.Suspense fallback={<div>Loading Page...</div>}>
+				<Switch>
+					<Route exact path='/' component={HomePage} />
+					<Route exact path='/about' component={AboutPage} />
+				</Switch>
+			</React.Suspense>
+		</Router>
 	);
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+/**
+ * @param {*} props
+ * @return {*}
+ */
+export default function HomePage() {
+	return <h3>Home Page</h3>;
+}

eb3d3740f9230228127e91db80b12392ae34c8e1
habbey sesan <habbeyunik@gmail.com>
2019-10-25 11:35:42 +0100

    setup eslint and prettier configs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * @params { Object } props
+ * @return { any } children
+ */
 function App() {
   return (
     <div className="App">
